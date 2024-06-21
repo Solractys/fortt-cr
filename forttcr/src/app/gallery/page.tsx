e563c740a204cbcd84f29904a1fba6b408186b6d
Felipe Matheus dos Santos <163012260+felipemths12@users.noreply.github.com>
2024-06-21 11:04:34 -0300

    Caixa e padding incluidos

--- forttcr/src/app/gallery/page.tsx
+++ forttcr/src/app/gallery/page.tsx
@@ -1,21 +1,21 @@
 import Navbar from "../components/Navbar";
 import styles from "./page.module.css";
-function Gallery  (){
+function Gallery (){
   return (
     <>
       <Navbar/>
       {/* Daqui pra baixo é contigo pae! */}
      <section className={styles.grid_image}>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
-    <div className={styles.image}>a</div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
+    <div className={styles.image}></div>
      </section>
     </>
   );
