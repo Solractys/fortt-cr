75fee845b37491bba99aba739a27f6fbdd17cae2
Solractys <carloseduardosilvasouzadelima@gmail.com>
2024-06-24 21:39:49 -0300

    fix gallery component

--- forttcr/src/app/servicos/page.tsx
+++ forttcr/src/app/servicos/page.tsx
@@ -15,65 +15,51 @@ function Servicos() {
                 <h1 className={styles.title}>CONHEÇA NOSSOS SERVIÇOS</h1>
                 <div className={styles.service_container}>
                     <div className={styles.service}>
-                        <div className={styles.icon}>
-                            <Image
+                            <Image className={styles.icon}
                                 src={pintura}
                                 alt="Imagem onde mostra uma parede pintada de amarelo com uma porta posicionada à esquerda e uma janela posicionada à direita."
                                 width={300}
-                                height={300}
                             />
-                        </div>
                         <h2>PINTURA</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
                         <button className={styles.btn}>CONTRATE AGORA!</button>
                     </div>
                     <div className={styles.service}>
-                        <div className={styles.icon}>
-                            <Image
+                            <Image className={styles.icon}
                                 src={telhado}
                                 alt="Imagem onde mostra um homem de camisa de manga longa cinza e calça azul realizando a pintura das telhas em um telhado branco."
                                 width={300}
-                                height={300}
                             />
-                        </div>
                         <h2>TELHADO</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
                         <button className={styles.btn}>CONTRATE AGORA!</button>
                     </div>
                     <div className={styles.service}>
-                        <div className={styles.icon}>
-                            <Image
+                            <Image className={styles.icon}
                                 src={reforma}
-                                alt="Imagem"
+                                alt="Demostração de serviço"
                                 width={300}
-                                height={300}
                             />
-                        </div>
                         <h2>REFORMAS</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
                         <button className={styles.btn}>CONTRATE AGORA!</button>
                     </div>
                     <div className={styles.service}>
-                        <div className={styles.icon}></div>
                         <h2>IMPERMEABILIZAÇÃO</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
                         <button className={styles.btn}>CONTRATE AGORA!</button>
                     </div>
                     <div className={styles.service}>
-                        <div className={styles.icon}>
-                            <Image
+                            <Image className={styles.icon}
                                 src={construcao}
-                                alt="Imagem"
+                                alt="Demonstração de serviço"
                                 width={300}
-                                height={300}
                             />
-                        </div>
                         <h2>CONSTRUÇÃO</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
                         <button className={styles.btn}>CONTRATE AGORA!</button>
                     </div>
                     <div className={styles.service}>
-                        <div className={styles.icon}></div>
                         <h2>GRADES</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
                         <button className={styles.btn}>CONTRATE AGORA!</button>
